8c3b84b376a201401e052b50051d9821f51c98e0
Anslem Coelho <anslem1999@gmail.com>
2025-07-17 19:55:33 -0400

    Add project files.

---
+++ Cumulative Assignment/Controllers/TeacherAPIController.cs
@@ -0,0 +1,63 @@
+﻿using Cumulative_Assignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+//using MySqlX.ResultSet;
+
+
+namespace Cumulative_Assignment.Controllers
+{
+    [Route("api/teacher")]
+    [ApiController]
+    public class TeacherAPIController : ControllerBase
+    {
+
+        private SchoolDbContext _context = new SchoolDbContext();
+
+        //private readonly SchoolDbContext _context;
+
+        //public TeacherAPIController(SchoolDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        [HttpGet(template:"ListTeacherInfo")]
+
+        public List<string> ListTeacherInfo()
+        {
+            List <string> TeacherInfo = new List<string>();
+
+            using (MySqlConnection Connection = _context.AccessDatabase())
+            {
+                Connection.Open();
+
+                string query = "select * from teachers";
+
+                MySqlCommand Command = Connection.CreateCommand(); ;
+
+                Command.CommandText = query;
+
+                using (MySqlDataReader ResultSet = Command.ExecuteReader())
+                {
+                    while (ResultSet.Read())
+                    {
+                        int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                        string TeacherFName = ResultSet["teacherfname"].ToString();
+                        string TeacherLName = ResultSet["teacherlname"].ToString();
+                        Decimal Salary = Decimal.Parse(ResultSet["salary"].ToString());
+                        DateTime HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
+                        string EmployeeNumber = ResultSet["employeenumber"].ToString();
+                        TeacherInfo.Add(TeacherId + TeacherFName + TeacherLName + Salary + HireDate + EmployeeNumber);
+                        ////TeacherInfo.Add(EmployeeNumber);
+                        
+                    }
+                }
+            }
+            return TeacherInfo;
+        }
+
+
+ 
+    }
+
+}
---
+++ Cumulative Assignment/Controllers/TeacherPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cumulative_Assignment.Controllers
+{
+    public class TeacherPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
