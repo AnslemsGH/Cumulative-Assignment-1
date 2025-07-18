019128325952cf3333170927c497a6e7922be73f
Anslem Coelho <anslem1999@gmail.com>
2025-07-18 09:35:42 -0400

    problem 2

--- Cumulative Assignment/Controllers/TeacherAPIController.cs
+++ Cumulative Assignment/Controllers/TeacherAPIController.cs
@@ -23,9 +23,9 @@ namespace Cumulative_Assignment.Controllers
 
         [HttpGet(template:"ListTeacherInfo")]
 
-        public List<string> ListTeacherInfo()
+        public List<Teacher> ListTeacherInfo()
         {
-            List <string> TeacherInfo = new List<string>();
+            List <Teacher> TeacherInfo = new List<Teacher>();
 
             using (MySqlConnection Connection = _context.AccessDatabase())
             {
@@ -41,15 +41,26 @@ namespace Cumulative_Assignment.Controllers
                 {
                     while (ResultSet.Read())
                     {
-                        int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
-                        string TeacherFName = ResultSet["teacherfname"].ToString();
-                        string TeacherLName = ResultSet["teacherlname"].ToString();
-                        Decimal Salary = Decimal.Parse(ResultSet["salary"].ToString());
-                        DateTime HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
-                        string EmployeeNumber = ResultSet["employeenumber"].ToString();
-                        TeacherInfo.Add(TeacherId + TeacherFName + TeacherLName + Salary + HireDate + EmployeeNumber);
+                        //int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                        //string TeacherFName = ResultSet["teacherfname"].ToString();
+                        //string TeacherLName = ResultSet["teacherlname"].ToString();
+                        //Decimal Salary = Decimal.Parse(ResultSet["salary"].ToString());
+                        //DateTime HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
+                        //string EmployeeNumber = ResultSet["employeenumber"].ToString();
+                        //TeacherInfo.Add(TeacherId + TeacherFName + TeacherLName + Salary + HireDate + EmployeeNumber);
                         ////TeacherInfo.Add(EmployeeNumber);
+
+
+                        Teacher Teacherinfo = new Teacher();
+                        Teacherinfo.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                        Teacherinfo.TeacherFName = ResultSet["teacherfname"].ToString();
+                        Teacherinfo.TeacherLName = ResultSet["teacherlname"].ToString();
+                        Teacherinfo.Salary = Decimal.Parse(ResultSet["salary"].ToString());
+                        Teacherinfo.HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
+                        Teacherinfo.EmployeeNumber = ResultSet["employeenumber"].ToString();
                         
+                        TeacherInfo.Add(Teacherinfo);
+
                     }
                 }
             }
@@ -57,7 +68,50 @@ namespace Cumulative_Assignment.Controllers
         }
 
 
- 
+
+        [HttpGet(template: "ATeacherInfo/{teachersid}")]
+
+        public ActionResult<List<Teacher>> ATeacherInfo(int teachersid)
+        {
+            List<Teacher> TeacherInfo = new List<Teacher>();
+
+            using (MySqlConnection Connection = _context.AccessDatabase())
+            {
+                Connection.Open();
+
+                string query = $"select * from teachers where teacherid = {teachersid}";
+
+                MySqlCommand Command = Connection.CreateCommand(); ;
+
+                Command.CommandText = query;
+
+                using (MySqlDataReader ResultSet = Command.ExecuteReader())
+                {
+                    if(ResultSet.Read())
+                    {                      
+                        Teacher Teacherinfo = new Teacher();
+                        Teacherinfo.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                        Teacherinfo.TeacherFName = ResultSet["teacherfname"].ToString();
+                        Teacherinfo.TeacherLName = ResultSet["teacherlname"].ToString();
+                        Teacherinfo.Salary = Decimal.Parse(ResultSet["salary"].ToString());
+                        Teacherinfo.HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
+                        Teacherinfo.EmployeeNumber = ResultSet["employeenumber"].ToString();
+
+                        TeacherInfo.Add(Teacherinfo);
+                        return TeacherInfo;
+
+                    }
+                    else
+                    {
+                        string message = "TeacherId input is incorrect as no teacher available is linked to the id";
+                        return NotFound(message);
+                    }
+                }
+            }
+
+        }
+
+
     }
 
 }
